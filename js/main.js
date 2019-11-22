1ef6fb491da97038980377e0f59fbb9f522f66f1
ketanharchand <ketan171921.cse@chitkara.edu.in>
2019-11-23 01:00:00 +0530

    PROJECT

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function fetch(){
     let text = 'Visit Profile'
     $.ajax({
         type:'GET',
-        url:`https:api.github.com/users/${user}?client_id=Iv1.27d23b109a50754c&client_secret=82015423e2a368c16cb2fbf9f6290d9fe8c77f70`,
+        url:'https://api.github.com/users/'+user+'?client_id=Iv1.27d23b109a50754c&client_secret=82015423e2a368c16cb2fbf9f6290d9fe8c77f70',
         success:function(response){
             let name = response.name || `Name is not available`
             let bio = response.bio || 'Bio is not available'
@@ -14,41 +14,54 @@ function fetch(){
                 return(
                     `
 
-                    <div class="card my-3" style="width:18rem">
+                    <div class="container">
+                    <div class="row">
             
-                        <div class="card-body">
+                        <div class="col-sm-3 pt-1" style="border:2px solid white;border-radius:5px;margin-bottom:40px">
 
                         <img src=${response.avatar_url} width="100%" alt="User's Images"/>
-
-                            <h5 class="card-title mt-2">
+                        <h5 class="mt-2 text-white">
                                Username:&nbsp; ${response.login}
                             </h5>
 
-                            <p class="card-text mt-3">
+                        </div>
+                        <div class="col-sm-4" style="border:2px solid white;border-radius:5px;margin-bottom:40px">
+
+                            
+                            <p class="text-white mt-3">
                                 Name:&nbsp;${name}
                             </p>
 
-                            <p class="card-text mt-3">
+                            <p class="text-white mt-3">
                                 Bio:&nbsp;${bio}
                             </p>
                             
                             
-                            <p class="card-text mt-3">
-                               Repos:&nbsp; ${response.public_repos}&nbsp;&nbsp;<button=class="btn btn-danger"><a href="http://github.com/${user}/repositories">Get Repositories</a></button>
+                            <p class="text-white mt-3">
+                               Repos:&nbsp; ${response.public_repos}&nbsp;&nbsp;<button class="btn btn-outline-danger"><a href="http://github.com/${user}/repositories">Get Repositories</a></button>
                             </p>
-                            <p class="card-text mt-3">
-                               Followers:&nbsp; ${response.followers}&nbsp;&nbsp;<button=class="btn btn-danger"><a href="http://github.com/${user}/followers">Get Followers</a></button>
+                            <p class="text-white mt-3">
+                               Followers:&nbsp; ${response.followers}&nbsp;&nbsp;<button class="btn btn-outline-danger"><a href="http://github.com/${user}/followers">Get Followers</a></button>
                              </p>
                              
-                             <p class="card-text mt-3">
-                               Following:&nbsp; ${response.following}&nbsp;&nbsp;<button=class="btn btn-danger"><a href="http://github.com/${user}/following">Get Following</a></button>
+                             <p class="text-white mt-3">
+                               Following:&nbsp; ${response.following}&nbsp;&nbsp;<button class="btn btn-outline-danger"><a href="http://github.com/${user}/following">Get Following</a></button>
                             </p>
                           
                             <br>
-                            
-                        <button class="mt-2 btn btn-mat text-white">
+                            </div>
+                            <div class="col-sm-5 d-flex p-3" style="border:2px solid white;border-radius:5px;margin-bottom:40px">
+                            <div >                      
+                            <button class=" btn btn-mat text-white" style="margin-top:100px">
                                 ${text.link(`${response.html_url}`)}
                         </button>
+                        </div>
+                        <div class="ml-2">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL_ADgdpn_jOSfr450lnKUQYpHkvY-gTx0i5UaLiEyQlTozimzwQ&s">
+                        </div>
+                        </div>
+                        </div>
+                        </div>
                         </div>
                     </div>
                     `
